9b031374beba1585dcc72945d07a18342b8bad24
M.KIRAN KUMAR REDDY <kiran.minpuri@gmail.com>
2026-03-30 17:47:29 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -0,0 +1,12 @@
+//bruteforce approach (nested loops)
+// time Complexity would be O(n^2),Spcae complexity O(1)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target) return new int[]{i,j};
+            }
+        }
+        return new int[]{0,0};
+    }
+}
